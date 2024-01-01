564f2e730bef358d61e1249204ba55c0c7b0dcaa
Stephen Heumann <stephenheumann@gmail.com>
2023-12-31 18:08:29 -0600

    Initial implementation of GetDevNumber.
    This allows the system to work with smb.fst installed, but no hfs.fst.

--- fstops/GetDevNumber.c
+++ fstops/GetDevNumber.c
@@ -1,7 +1,28 @@
 #include "defs.h"
 #include <gsos.h>
 #include <prodos.h>
+#include "fstdata.h"
 
-Word GetDevNumber(void *pblock, void *gsosdp, Word pcount) {
-    return invalidFSTop;
+Word GetDevNumber(void *pblock, struct GSOSDP *gsosdp, Word pcount) {
+    if (gsosdp->dev1Num == 0) {
+        // TODO is there a success case where we need to look up the dev num?
+        // TODO adjust error based on type of path (device or volume name?)
+        return devNotFound;
+    }
+    
+    if (pcount == 0) {
+        #define pblock ((DevNumRec*)pblock)
+        
+        pblock->devNum = gsosdp->dev1Num;
+        
+        #undef pblock
+    } else {
+        #define pblock ((DevNumRecGS*)pblock)
+        
+        pblock->devNum = gsosdp->dev1Num;
+        
+        #undef pblock
+    }
+
+    return 0;
 }

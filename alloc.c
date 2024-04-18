bc7c113c721829a0b7f3317e9153fde38e418be7
Stephen Heumann <stephenheumann@gmail.com>
2024-04-17 20:01:41 -0500

    Use our own allocator code, rather than malloc/free.
    The new allocator tries to allocate our memory in banks E0 or E1 if possible, so that it does not fragment the main memory used by applications.

---
+++ alloc.c
@@ -0,0 +1,35 @@
+#include "defs.h"
+#include <memory.h>
+#include <orca.h>
+#include "alloc.h"
+
+void *smb_malloc(size_t size) {
+    Handle handle;
+    Word attributes = attrLocked | attrFixed | attrNoSpec;
+    
+    if (size < 0x10000)
+        attributes |= attrNoCross;
+    
+    /*
+     * First try to allocate in bank $E0 or $E1 to avoid fragmenting regular
+     * memory.  If that fails, just allocate anywhere non-special.
+     */
+    handle = NewHandle(size, userid(), attributes | attrBank, (void*)0xE00000);
+    if (!toolerror())
+        return *handle;
+
+    handle = NewHandle(size, userid(), attributes | attrBank, (void*)0xE10000);
+    if (!toolerror())
+        return *handle;
+
+    handle = NewHandle(size, userid(), attributes, 0);
+    if (!toolerror())
+        return *handle;
+
+    return 0;
+}
+
+
+void smb_free(void *ptr) {
+    DisposeHandle(FindHandle(ptr));
+}

2aa1d6a80699b1d31f439f5740af48b1e2c36e30
Stephen Heumann <stephenheumann@gmail.com>
2024-04-18 18:50:41 -0500

    Implement JudgeName.

--- fstops/JudgeName.c
+++ fstops/JudgeName.c
@@ -2,7 +2,71 @@
 #include <gsos.h>
 #include <prodos.h>
 #undef JudgeName
+#include "gsosdata.h"
+#include "fstspecific.h"
+#include <misctool.h>
 
-Word JudgeName(void *pblock, void *gsosdp, Word pcount) {
-    return invalidFSTop;
+// JudgeName name types
+#define NAME_TYPE_UNKNOWN        0
+#define NAME_TYPE_VOLUME_NAME    1
+#define NAME_TYPE_DIRECTORY_NAME 2
+#define NAME_TYPE_FILE_NAME      3
+
+// JudgeName flag bits
+#define FLAG_NAME_CONTAINED_ILLEGAL_CHARACTERS 0x8000
+#define FLAG_NAME_TOO_LONG                     0x4000
+#define FLAG_SYNTAX_ERROR                      0x2000
+
+Word JudgeName(JudgeNameRecGS *pblock, struct GSOSDP *gsosdp, Word pcount) {
+    Word flags;
+    Word i;
+    ResultBuf *name;
+
+    if (pblock->fileSysID != smbFSID)
+        return invalidFSTID;
+
+    if (pblock->nameType > NAME_TYPE_FILE_NAME)
+        return paramRangeErr;
+    
+    pblock->syntax =
+        "\pSMB names may contain up to 255 Unicode characters. "
+        "Some servers may disallow certain names.";
+
+    if (pcount >= 4) {
+        pblock->maxLen = 255;
+
+    if (pcount >= 5) {
+        flags = 0;
+
+        name = pblock->name;
+        if (name != NULL) {
+            /*
+             * Other FSTs require a big enough buffer to fit a max-length name.
+             * We intentionally do not, since 
+             * (a) we do not expand the size of names (except "" to "A"), and
+             * (b) existing programs may use smaller buffers.
+             */
+            if (name->bufSize < 5)
+                return buffTooSmall;
+       
+            if (name->bufString.length == 0) {
+                name->bufString.length = 1;
+                name->bufString.text[0] = 'A';
+                flags = FLAG_SYNTAX_ERROR;
+            } else {
+                for (i = 0; i < name->bufString.length; i++) {
+                    if (name->bufString.text[i] == 0
+                        || name->bufString.text[i] == ':') {
+                        name->bufString.text[i] = '_';
+                        flags |= FLAG_NAME_CONTAINED_ILLEGAL_CHARACTERS;
+                    }
+                }
+            }
+        }
+    
+    if (pcount >= 6) {
+        pblock->nameFlags = flags;
+    }}}
+
+    return 0;
 }

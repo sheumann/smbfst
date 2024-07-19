c7a65e7fb16796e621cf4eb33f6303e9a1682ad1
Stephen Heumann <stephenheumann@gmail.com>
2024-07-19 13:23:43 -0500

    Bump version number for 1.0 release.

--- fst/smbfst.asm
+++ fst/smbfst.asm
@@ -25,7 +25,7 @@ fstheader start
         dc      a4'sys_entry'           ; system entry point
         dc      i2'SMB_FST_ID'          ; FST ID
         dc      i2'$0002'               ; attributes: non-apple format
-        dc      i2'$8100'               ; version 1.0 (prototype)
+        dc      i2'$0100'               ; version 1.0
         dc      i2'512'                 ; block size
         dc      i4'$ffffffff'           ; max volume size in blocks
         dc      i4'1'                   ; min volume size in blocks
@@ -33,7 +33,7 @@ fstheader start
         dc      i4'0'                   ; reserved
         dc      i1'3',c'SMB'            ; FST name (p-string)
 ;                                       ; FST info string (shown in text boot)
-        dc      i1'35',c'SMB FST               v01.00 beta 3'
+        dc      i1'28',c'SMB FST               v01.00'
         dc      i2'0'
 ;                                       ; credits for FST
         dc      i1'26',c'SMB FST by Stephen Heumann'
